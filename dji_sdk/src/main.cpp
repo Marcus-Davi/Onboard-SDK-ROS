99743a9ad7d6eb24577f04334b2b7ec702d3227e
Marcus <davi2812@dee.ufc.br>
2020-02-28 16:59:59 -0300

    comment

--- dji_sdk/src/main.cpp
+++ dji_sdk/src/main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
   ros::NodeHandle nh_private("~");
 
+  //Added bool test
   DJISDKNode* dji_sdk_node = new DJISDKNode(nh, nh_private);
   if(dji_sdk_node->InitSucess == false){
     delete dji_sdk_node;
